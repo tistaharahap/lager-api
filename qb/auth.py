1aa057f035a1248902625166a34ba6e96ee0a9a6
Batista Harahap <batista@bango29.com>
2016-11-09 17:59:19 +0700

    disable auth

--- qb/auth.py
+++ qb/auth.py
@@ -18,15 +18,16 @@ def get_redis_conn(host='192.168.99.100', port=6379, db=7):
 def init_auth(app, error):
     @app.middleware('request')
     async def authorize_request(request):
-        token = request.headers.get('X-Access-Token')
-        if not token:
-            raise error('All requests must be set with X-Access-Token header')
+        # token = request.headers.get('X-Access-Token')
+        # if not token:
+        #     raise error('All requests must be set with X-Access-Token header')
 
-        redis_conn = get_redis_conn(host='127.0.0.1',
-                                    port=6379,
-                                    db=7)
+        # redis_conn = get_redis_conn(host='127.0.0.1',
+        #                             port=6379,
+        #                             db=7)
 
-        authorized = authorize(redis_conn=redis_conn, 
-                               token=token)
-        if not authorized:
-            raise error('Invalid token')
+        # authorized = authorize(redis_conn=redis_conn, 
+        #                        token=token)
+        # if not authorized:
+        #     raise error('Invalid token')
+        pass
