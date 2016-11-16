ab3e8db70236784f7db1333cbfac06152647d932
Batista Harahap <batista@bango29.com>
2016-11-16 23:57:32 +0700

    add content to get route

--- qb/handlers/routesearch.py
+++ qb/handlers/routesearch.py
@@ -5,21 +5,30 @@ from qb.elasticsearch import get_es_connection
 
 
 async def handle_route_search(request):
-	config = request.json.get('config')
-	get_es_connection(hosts=config.get('elasticsearch').get('hosts'))
-	skyscanner_token = config.get('skyscanner').get('token')
+    config = request.json.get('config')
+    get_es_connection(hosts=config.get('elasticsearch').get('hosts'))
+    skyscanner_token = config.get('skyscanner').get('token')
 
-	meta = request.json.get('meta')
+    meta = request.json.get('meta')
 
-	origin_airport = await Airport.get_airport_by_iata_code(iata_code=meta.get('origin'))
-	destination_airport = await Airport.get_airport_by_iata_code(iata_code=meta.get('destination'))
-	outbound_date = meta.get('outbound_date')
-	inbound_date = meta.get('inbound_date')
+    origin_airport = await Airport.get_airport_by_iata_code(iata_code=meta.get('origin'))
+    destination_airport = await Airport.get_airport_by_iata_code(iata_code=meta.get('destination'))
+    outbound_date = meta.get('outbound_date')
+    inbound_date = meta.get('inbound_date')
 
-	flight = await search_flights(origin=origin_airport,
-						   		  destination=destination_airport,
-						   		  departure_date=outbound_date,
-						   		  returning_date=inbound_date,
-						   		  skyscanner_token=skyscanner_token)
+    flight = await search_flights(origin=origin_airport,
+                                  destination=destination_airport,
+                                  departure_date=outbound_date,
+                                  returning_date=inbound_date,
+                                  skyscanner_token=skyscanner_token)
+    
+    flight.update({
+        'contents': {
+            'picture': destination_airport.get('image') if destination_airport else '',
+            'description': destination_airport.get('description') if destination_airport else '',
+            'wikipedia_url': 'http://%s' % destination_airport.get('wikipedia_url') if destination_airport else '',
+            'location': destination_airport.get('location')
+        }
+    })
 
-	return dict(data=flight)
+    return dict(data=flight)
