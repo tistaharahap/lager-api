8d67735aaace14f4147a0e163184a1d15fca28e8
Batista Harahap <batista@bango29.com>
2016-11-17 02:42:49 +0700

    add origin to cron

--- cron.sh
+++ cron.sh
@@ -1,9 +1,33 @@
 #!/bin/sh
+echo 'IDR 1mio\n'
 curl -H "Content-Type: application/json" -X POST -d '{"action":"flight-search-with-budget","actor":{"id":"@tista","kind":"person"},"meta":{"number":1000000,"currency":"IDR","passengers":{"adults":1,"children":0,"infants":0},"dates":{"outbound":"2016-11-18","inbound":"2016-11-20"},"origin": "CGK-sky"},"object":{"id":"IDR-1500000","kind":"currency-number"}}' https://api.travelonbudget.co/v1/events
+echo 'IDR 2mio\n'
 curl -H "Content-Type: application/json" -X POST -d '{"action":"flight-search-with-budget","actor":{"id":"@tista","kind":"person"},"meta":{"number":2000000,"currency":"IDR","passengers":{"adults":1,"children":0,"infants":0},"dates":{"outbound":"2016-11-18","inbound":"2016-11-20"},"origin": "CGK-sky"},"object":{"id":"IDR-1500000","kind":"currency-number"}}' https://api.travelonbudget.co/v1/events
+echo 'IDR 3mio\n'
 curl -H "Content-Type: application/json" -X POST -d '{"action":"flight-search-with-budget","actor":{"id":"@tista","kind":"person"},"meta":{"number":3000000,"currency":"IDR","passengers":{"adults":1,"children":0,"infants":0},"dates":{"outbound":"2016-11-18","inbound":"2016-11-20"},"origin": "CGK-sky"},"object":{"id":"IDR-1500000","kind":"currency-number"}}' https://api.travelonbudget.co/v1/events
+echo 'IDR 4mio\n'
 curl -H "Content-Type: application/json" -X POST -d '{"action":"flight-search-with-budget","actor":{"id":"@tista","kind":"person"},"meta":{"number":4000000,"currency":"IDR","passengers":{"adults":1,"children":0,"infants":0},"dates":{"outbound":"2016-11-18","inbound":"2016-11-20"},"origin": "CGK-sky"},"object":{"id":"IDR-1500000","kind":"currency-number"}}' https://api.travelonbudget.co/v1/events
+echo 'IDR 5mio\n'
 curl -H "Content-Type: application/json" -X POST -d '{"action":"flight-search-with-budget","actor":{"id":"@tista","kind":"person"},"meta":{"number":5000000,"currency":"IDR","passengers":{"adults":1,"children":0,"infants":0},"dates":{"outbound":"2016-11-18","inbound":"2016-11-20"},"origin": "CGK-sky"},"object":{"id":"IDR-1500000","kind":"currency-number"}}' https://api.travelonbudget.co/v1/events
+echo 'IDR 6mio\n'
 curl -H "Content-Type: application/json" -X POST -d '{"action":"flight-search-with-budget","actor":{"id":"@tista","kind":"person"},"meta":{"number":6000000,"currency":"IDR","passengers":{"adults":1,"children":0,"infants":0},"dates":{"outbound":"2016-11-18","inbound":"2016-11-20"},"origin": "CGK-sky"},"object":{"id":"IDR-1500000","kind":"currency-number"}}' https://api.travelonbudget.co/v1/events
+echo 'IDR 7mio\n'
 curl -H "Content-Type: application/json" -X POST -d '{"action":"flight-search-with-budget","actor":{"id":"@tista","kind":"person"},"meta":{"number":7000000,"currency":"IDR","passengers":{"adults":1,"children":0,"infants":0},"dates":{"outbound":"2016-11-18","inbound":"2016-11-20"},"origin": "CGK-sky"},"object":{"id":"IDR-1500000","kind":"currency-number"}}' https://api.travelonbudget.co/v1/events
+echo 'IDR 8mio\n'
 curl -H "Content-Type: application/json" -X POST -d '{"action":"flight-search-with-budget","actor":{"id":"@tista","kind":"person"},"meta":{"number":8000000,"currency":"IDR","passengers":{"adults":1,"children":0,"infants":0},"dates":{"outbound":"2016-11-18","inbound":"2016-11-20"},"origin": "CGK-sky"},"object":{"id":"IDR-1500000","kind":"currency-number"}}' https://api.travelonbudget.co/v1/events
+echo 'IDR 9mio\n'
+curl -H "Content-Type: application/json" -X POST -d '{"action":"flight-search-with-budget","actor":{"id":"@tista","kind":"person"},"meta":{"number":9000000,"currency":"IDR","passengers":{"adults":1,"children":0,"infants":0},"dates":{"outbound":"2016-11-18","inbound":"2016-11-20"},"origin": "CGK-sky"},"object":{"id":"IDR-1500000","kind":"currency-number"}}' https://api.travelonbudget.co/v1/events
+echo 'IDR 10mio\n'
+curl -H "Content-Type: application/json" -X POST -d '{"action":"flight-search-with-budget","actor":{"id":"@tista","kind":"person"},"meta":{"number":10000000,"currency":"IDR","passengers":{"adults":1,"children":0,"infants":0},"dates":{"outbound":"2016-11-18","inbound":"2016-11-20"},"origin": "CGK-sky"},"object":{"id":"IDR-1500000","kind":"currency-number"}}' https://api.travelonbudget.co/v1/events
+echo 'IDR 11mio\n'
+curl -H "Content-Type: application/json" -X POST -d '{"action":"flight-search-with-budget","actor":{"id":"@tista","kind":"person"},"meta":{"number":11000000,"currency":"IDR","passengers":{"adults":1,"children":0,"infants":0},"dates":{"outbound":"2016-11-18","inbound":"2016-11-20"},"origin": "CGK-sky"},"object":{"id":"IDR-1500000","kind":"currency-number"}}' https://api.travelonbudget.co/v1/events
+echo 'IDR 12mio\n'
+curl -H "Content-Type: application/json" -X POST -d '{"action":"flight-search-with-budget","actor":{"id":"@tista","kind":"person"},"meta":{"number":12000000,"currency":"IDR","passengers":{"adults":1,"children":0,"infants":0},"dates":{"outbound":"2016-11-18","inbound":"2016-11-20"},"origin": "CGK-sky"},"object":{"id":"IDR-1500000","kind":"currency-number"}}' https://api.travelonbudget.co/v1/events
+echo 'IDR 13mio\n'
+curl -H "Content-Type: application/json" -X POST -d '{"action":"flight-search-with-budget","actor":{"id":"@tista","kind":"person"},"meta":{"number":13000000,"currency":"IDR","passengers":{"adults":1,"children":0,"infants":0},"dates":{"outbound":"2016-11-18","inbound":"2016-11-20"},"origin": "CGK-sky"},"object":{"id":"IDR-1500000","kind":"currency-number"}}' https://api.travelonbudget.co/v1/events
+echo 'IDR 14mio\n'
+curl -H "Content-Type: application/json" -X POST -d '{"action":"flight-search-with-budget","actor":{"id":"@tista","kind":"person"},"meta":{"number":14000000,"currency":"IDR","passengers":{"adults":1,"children":0,"infants":0},"dates":{"outbound":"2016-11-18","inbound":"2016-11-20"},"origin": "CGK-sky"},"object":{"id":"IDR-1500000","kind":"currency-number"}}' https://api.travelonbudget.co/v1/events
+echo 'IDR 15mio\n'
+curl -H "Content-Type: application/json" -X POST -d '{"action":"flight-search-with-budget","actor":{"id":"@tista","kind":"person"},"meta":{"number":15000000,"currency":"IDR","passengers":{"adults":1,"children":0,"infants":0},"dates":{"outbound":"2016-11-18","inbound":"2016-11-20"},"origin": "CGK-sky"},"object":{"id":"IDR-1500000","kind":"currency-number"}}' https://api.travelonbudget.co/v1/events
+echo 'IDR 16mio\n'
+curl -H "Content-Type: application/json" -X POST -d '{"action":"flight-search-with-budget","actor":{"id":"@tista","kind":"person"},"meta":{"number":16000000,"currency":"IDR","passengers":{"adults":1,"children":0,"infants":0},"dates":{"outbound":"2016-11-18","inbound":"2016-11-20"},"origin": "CGK-sky"},"object":{"id":"IDR-1500000","kind":"currency-number"}}' https://api.travelonbudget.co/v1/events
